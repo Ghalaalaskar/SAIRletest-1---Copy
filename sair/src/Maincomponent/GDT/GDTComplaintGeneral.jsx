a10089d92cb5a3f9abe443d6510fc20eef8bda11
RawanSaud23 <srawanalsaif@gmail.com>
2025-05-04 02:00:38 +0300

    ..

--- sair/src/Maincomponent/GDT/GDTComplaintGeneral.jsx
+++ sair/src/Maincomponent/GDT/GDTComplaintGeneral.jsx
@@ -402,7 +402,7 @@ const GDTComplaintGeneral = () => {
     }
   };
 
-  // Determine the active state for the Header rawan
+  // Determine the active state for the Header
   let activeHeader;
   if (from === "GDTViolationDetail") {
     activeHeader = location.state?.previousList || "gdtcomplaints"; // Default to 'complaints' if not set
@@ -438,7 +438,6 @@ const GDTComplaintGeneral = () => {
           )}
           {from === "GDTViolationDetails" && (
             <>
-              {/* rawan */}
               <a onClick={() => navigate("/gdtdriverslist")}>Driver List</a>
               <span> / </span>
               <a
