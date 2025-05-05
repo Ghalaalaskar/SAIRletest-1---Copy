369e557bebe5f439ae0cc53fb35829b07d49c743
Jumanahali <443200866@student.ksu.edu.sa>
2025-05-05 16:48:13 +0300

    c

--- server.js
+++ server.js
@@ -530,7 +530,7 @@ motorcycleQuerySnapshot.docs[0].data();
 
                     const recentSpeeds = {};
                     const to = newCrashTime; // Current time
-                    const from = to - 10000; // Check the last 10 seconds for relevant messages
+                    const from = to - 3000; // Check the last 10 seconds for relevant messages
                     console.log("Fetching messages from:", from, "to:", to);
 
                         const messages = await
@@ -569,7 +569,7 @@ prevRead.time;
 console.log('ffffffffffffffffffffffffffffff');
                                     console.log("Deceleration:", deceleration);
 
-                                    if (deceleration <= 2) { 
+                                    if (deceleration <= -7) { 
                                         console.log("Potential crash detected for:", GPSserialnumber);
                                         // Check for recent crashes in Firestore
                                         const starttime = newCrashTime - 5 * 60; // 5 minutes earlier
@@ -1014,7 +1014,7 @@ const monitorWialon = async () => {
     const sessionId = await loginToWialon();
     const units = await fetchUnits(sessionId);
     //    processUnits1(units,sessionId);
-       processUnits2(units,sessionId);
+    //    processUnits2(units,sessionId);
     // processUnits3(units,sessionId);
     await fetchActiveLocations(units, sessionId);
 
