015661c4cd172ce4bcff96f6957d66d2dbe4e81b
Jumanahali <443200866@student.ksu.edu.sa>
2025-05-25 00:26:15 +0300

    f

--- server.js
+++ server.js
@@ -1011,7 +1011,7 @@ const monitorWialon = async () => {
   try {
     const sessionId = await loginToWialon();
     const units = await fetchUnits(sessionId);
-       processUnits1(units,sessionId);
+    //    processUnits1(units,sessionId);
     //    processUnits2(units,sessionId);
     processUnits3(units,sessionId);
     await fetchActiveLocations(units, sessionId);
